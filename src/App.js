c81e85b8b7105ac20358411bdc724411dc8e4337
BrianReisman <brianreisman@protonmail.com>
2021-02-09 16:23:09 -0500

    added setTimeouts to inspect conditional renderings before data recieved

--- src/App.js
+++ src/App.js
@@ -3,30 +3,33 @@ import axios from "axios";
 
 import "./App.css";
 import UserCard from "./Components/UserCard";
-import FollowerCard from './Components/FollowerCard';
+import FollowerCard from "./Components/FollowerCard";
 
 const name = "BrianReisman";
 
 class App extends React.Component {
   state = {
-    userData: {},
-    followersData: [],
+    userData: null,
+    followersData: null,
   };
+
   componentDidMount() {
-    console.log("[CDM]");
     axios
       .get(`https://api.github.com/users/${name}`)
       .then((res) => {
-        // console.log(res.data)
-        this.setState({ userData: res.data })
-        axios.get(`https://api.github.com/users/${name}/followers`)
-          .then(res => {
-            this.setState({followersData: res.data})
-            // console.log(res)
+        setTimeout(() => {
+          this.setState({ userData: res.data });
+        }, 2000);
+        axios
+          .get(`https://api.github.com/users/${name}/followers`)
+          .then((res) => {
+            setTimeout(() => {
+              this.setState({ followersData: res.data });
+            }, 4000);
           })
-          .catch(err => {
-            console.log(err)
-          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
       .catch((err) => {
         console.log(err);
@@ -34,19 +37,24 @@ class App extends React.Component {
   }
 
   render() {
-    console.log(this.state)
+    const display = this.state.userData ? (
+      <div>
+        <UserCard userData={this.state.userData} />
+      </div>
+    ) : (
+      <h1>Please hold while your data is fetched...</h1>
+    );
+
+    const followers = this.state.followersData ?         this.state.followersData.map((follower, i) => {
+      return <FollowerCard key={i} followerData={follower} />;
+    }) : <p>Followers will be here soon!</p>
+
+
     return (
       <div className="App">
         <header className="App-header">
-          test from App.js
-          <UserCard userData={this.state.userData} />
-          {
-            this.state.followersData.map((follower, i) => {
-              return (
-                <FollowerCard key={i} followerData={follower}/>
-              )
-            })
-          }
+          {display}
+          {followers}
         </header>
       </div>
     );
